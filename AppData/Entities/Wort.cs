8c88432bcda5911391f1a4c1884f8f4b487f39e5
Emre Mumcu <emumcu@outlook.com>
2025-06-10 16:44:48 +0300

    Updates and fixes

--- AppData/Entities/Wort.cs
+++ AppData/Entities/Wort.cs
@@ -6,16 +6,18 @@ namespace Wörter.AppData.Entities;
 
 public class Wort: BaseEntity
 {
-	public Wortart Art { get; set; }
+	public Wortart Art { get; set; } // Word Type
 	public string DE { get; set; } = null!;
 	public string EN { get; set; } = null!;
 	public string TR { get; set; } = null!;
-	public string? Geschlecht { get; set; }	
-	public string? Aussprache { get; set; }	
-	public string? Plural { get; set; } 
-	public string? Verbkonjugation { get; set; } 
-	public string? Beispiel { get; set; }
-	public string? Erläuterung { get; set; }
+	public string? Geschlecht { get; set; } // Gender (for Nouns)
+	public string? Aussprache { get; set; } // Pronunciation
+	public string? Plural { get; set; }  // Plural (for Nouns)
+	public string? Verbkonjugation { get; set; } // Verb conjugation (for Verbs)
+	public string? Beispiel { get; set; } // Example
+	public string? Erläuterung { get; set; } // Explanation
+	public string? Synonym { get; set; } // Synonyms
+	public string? Gegenteil { get; set; } // Opposite
 }
 
 public class WortKonfiguration : IEntityTypeConfiguration<Wort>
---
+++ AppData/Migrations/20250610122417_Mig1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wörter.AppData.Migrations
+{
+    /// <inheritdoc />
+    public partial class Mig1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Gegenteil",
+                table: "Wörter",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Synonym",
+                table: "Wörter",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Gegenteil",
+                table: "Wörter");
+
+            migrationBuilder.DropColumn(
+                name: "Synonym",
+                table: "Wörter");
+        }
+    }
+}
